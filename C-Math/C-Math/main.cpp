ec54f42d944a37522682ef6a87cde1118a6089da
うい 椎名 <shiina04jbs10@outlook.com>
2026-05-23 01:18:14 +0900

    Add_Equalとrange_sum追加

--- C-Math/C-Math/main.cpp
+++ C-Math/C-Math/main.cpp
@@ -1,9 +1,13 @@
 #include"../Four arithmetic operations/Four arithmetic operations.h"
-
+#include<vector>
 
 int main() {
 
-	auto ans = normal_sum<float>(100, 200, 300,600.6f);
+	auto ans = normal_sum(100, 200, 300,600);
+
+	std::vector<int> vec_{ 10,20,30,40,50,60,70,80,90 };
+
+	auto ranges = ranges_sum(vec_);
 
 	return 0;
 }
--- C-Math/Four arithmetic operations/Four arithmetic operations.h
+++ C-Math/Four arithmetic operations/Four arithmetic operations.h
@@ -7,11 +7,27 @@ template<typename T>
 concept Add_able = requires(T a, T b) {
     { a + b } -> std::convertible_to<T>;
 };
+template<typename T>
+concept Add_Equal_able = requires(T a, T b) {
+    { a += b } -> std::convertible_to<T>;
+};
 
-template<Add_able T,Add_able... Args>
-T normal_sum(Args... args) {
+template<Add_able... Args>
+auto normal_sum(Args&&... args) {
+    auto ans = (... + args);
+    OutPut(ans);
+    return ans;
+}
+
+template<std::ranges::input_range R>
+requires Add_Equal_able<std::ranges::range_value_t<R>>
+auto ranges_sum(R&& r)
+{
+    using T = std::ranges::range_value_t<R>;
     T ans{};
-    ((ans += args), ...);
+    for (auto&& x : r) {
+        ans += x;
+    }
     OutPut(ans);
     return ans;
 }
